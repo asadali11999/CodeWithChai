d7d7b54688bb27212f822818567d2b0e37428879
Ali Asif <112097778+asadali11999@users.noreply.github.com>
2023-09-18 20:31:40 +0000

    obj art two

---
+++ 02_basics/03_Objects_concat.js
@@ -0,0 +1,23 @@
+let obj1 = {
+    2:"a",
+    3:"b"
+}
+let obj2 = {
+    4:"a",
+    5:"b"
+}
+//how to merge diffrent obj into one 
+
+// there are many ways bu these are two best ways 
+
+// let obj4 = Object.assign(obj1,obj2)
+
+
+// here the curly brases "{}" is th etarget where all the objs will asign rather than the first "obj1"
+let obj4 = Object.assign({},obj1,obj2)
+
+let obj3 = {...obj1 , ...obj2}
+
+
+console.log(obj3);
+console.log(obj4);
